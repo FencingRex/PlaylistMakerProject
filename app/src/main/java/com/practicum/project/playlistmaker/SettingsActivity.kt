495487f6be10b6b689ddc16c31416554d12c5f5d
FencingRex <phenix3legio@gmail.com>
2025-11-11 15:03:50 +0300

    playlistMaker project

--- app/src/main/java/com/practicum/project/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/SettingsActivity.kt
@@ -1,4 +1,12 @@
 package com.practicum.project.playlistmaker
 
-class SettingsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
 }
