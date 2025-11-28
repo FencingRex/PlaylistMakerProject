db0407af558f20179649b06f66aa74e05c9e97ad
FencingRex <phenix3legio@gmail.com>
2025-11-28 15:42:21 +0300

    playlistMaker project

--- app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.project.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +12,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        val mediaBtn = findViewById<Button>(R.id.media_btn)
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+
+        val mediaClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?){
+                Toast.makeText(this@MainActivity,"Button Media clicked", Toast.LENGTH_LONG).show()
+            }
+        }
+        mediaBtn.setOnClickListener(mediaClickListener)
+
+        searchBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button search tapped",Toast.LENGTH_LONG).show()
+        }
     }
 }
