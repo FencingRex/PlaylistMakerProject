d02409f9d9eb454178894a1c9b61814ac2918d8a
FencingRex <phenix3legio@gmail.com>
2025-12-15 17:12:04 +0300

    playlistMaker project правки по замечаниям

--- app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
@@ -2,18 +2,12 @@ package com.practicum.project.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
 
@@ -34,18 +28,6 @@ class MainActivity : AppCompatActivity() {
             val searchIntent = Intent(this, SearchActivity::class.java)
             startActivity(searchIntent)
         }
-        /*
-                val mediaClickListener: View.OnClickListener = object : View.OnClickListener {
-                    override fun onClick(v: View?){
-                        Toast.makeText(this@MainActivity,"Button Media clicked", Toast.LENGTH_LONG).show()
-                    }
-                }
 
-                mediaBtn.setOnClickListener(mediaClickListener)
-
-                searchBtn.setOnClickListener {
-                    Toast.makeText(this@MainActivity, "Button search tapped",Toast.LENGTH_LONG).show()
-                }
-        */
     }
 }
