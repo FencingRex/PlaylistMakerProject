75140f6d3bf73aa26738a927437b072d64f8b8ae
FencingRex <phenix3legio@gmail.com>
2025-12-29 17:52:05 +0300

    playlistMaker project sprint 9 settings activity style homework pt 2

--- app/src/main/java/com/practicum/project/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package com.practicum.project.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Layout
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,7 +14,42 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backBtn = findViewById<Button>(R.id.settings_back)
+        val shareBtn = findViewById<LinearLayout>(R.id.shareBtn)
+        val supportBtn = findViewById<LinearLayout>(R.id.supportBtn)
+        val userAgreementBtn = findViewById<LinearLayout>(R.id.userAgreemBtn)
 
         backBtn.setOnClickListener { finish() }
+
+        shareBtn.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            val url = getString(R.string.shareMsg)
+            shareIntent.putExtra(Intent.EXTRA_TEXT,url)
+            val chooserMsg = getString(R.string.chooserMsg)
+            startActivity(Intent.createChooser(shareIntent,  chooserMsg))
+        }
+
+        supportBtn.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                val mail = getString(R.string.supportMailTo)
+                val subject = getString(R.string.supportMailSubject)
+                val message =  getString(R.string.supportMailBody)
+
+                val msg = "mailto:$mail" +
+                        "?subject=${Uri.encode(subject)}" +
+                        "?body=${Uri.encode(message)}"
+                data = Uri.parse(msg)
+                putExtra(Intent.EXTRA_SUBJECT,subject)
+                putExtra(Intent.EXTRA_TEXT, message)
+
+            }
+            startActivity(Intent.createChooser(supportIntent,getString(R.string.support)))
+        }
+
+        userAgreementBtn.setOnClickListener {
+            val url = Uri.parse(getString(R.string.userAgreemLink))
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW,url)
+                startActivity(userAgreementIntent)
+        }
     }
 }
