e3f1c33194af20328f70cf14260fd9c7fdb3e4f0
FencingRex <phenix3legio@gmail.com>
2026-01-19 11:58:28 +0300

    playlistMaker project sprint 9 homework pt 5

--- app/src/main/java/com/practicum/project/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/SearchActivity.kt
@@ -1,20 +1,68 @@
 package com.practicum.project.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.content.Context
+import android.os.PersistableBundle
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private var searchQuery: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchBack = findViewById<Button>(R.id.searchBack)
+        val searchRequest = findViewById<EditText>(R.id.searchInputText)
+        val clearBtn = findViewById<ImageView>(R.id.clearIcon)
+
+        searchBack.setOnClickListener { finish() }
+
+        clearBtn.setOnClickListener {
+            searchRequest.setText("")
         }
+
+        val simpleTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+               //empty
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearBtn.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+                searchQuery = s.toString()
+            }
+        }
+        searchRequest.addTextChangedListener(simpleTextWatcher)
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int{
+        return if (s.isNullOrEmpty()){
+            View.GONE
+        }else{
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("savedSearchReq",editText.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchQuery = savedInstanceState.getString("savedSearchReq","")
+        editText.setText(searchQuery)
     }
 }
