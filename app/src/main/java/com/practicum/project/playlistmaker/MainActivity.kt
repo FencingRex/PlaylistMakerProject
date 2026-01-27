f895adcd03a09161144679bc3aa54e049e416f7e
FencingRex <phenix3legio@gmail.com>
2026-01-27 16:01:56 +0300

    playlistMaker project sprint 10 homework another one try

--- app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/MainActivity.kt
@@ -3,13 +3,15 @@ package com.practicum.project.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val mediaBtn = findViewById<Button>(R.id.btn_media)
         val searchBtn = findViewById<Button>(R.id.btn_search)
