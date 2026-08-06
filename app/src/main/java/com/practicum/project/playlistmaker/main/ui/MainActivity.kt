d523c552e0ed1f59580a6eb57258c3d5c86d46c8
FencingRex <phenix3legio@gmail.com>
2026-08-06 10:21:12 +0300

    playlistMaker project sprint 19 homework Fragment, BottomNavigatioMenu вернул systemBar

--- app/src/main/java/com/practicum/project/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/practicum/project/playlistmaker/main/ui/MainActivity.kt
@@ -16,8 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        enableEdgeToEdge()
-        window.decorView.apply { systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) }
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
 
         val navController = navHostFragment.navController
